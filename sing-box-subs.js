655c253659ac450b5fdeea8d6d4ec4e5a862f0b0
D1evie <zufar1635@icloud.com>
2025-09-13 00:37:28 +0300

    Update sing-box-subs.js

--- sing-box-subs.js
+++ sing-box-subs.js
@@ -10,7 +10,7 @@ let proxies = await produceArtifact({
 
 config.outbounds.push(...proxies);
 
-function pushToTargets(targetTags, tags) {
+function pushTo(targetTags, tags) {
   config.outbounds.forEach(o => {
     if (Array.isArray(o.outbounds) && targetTags.includes(o.tag)) {
       o.outbounds.push(...tags);
@@ -18,20 +18,16 @@ function pushToTargets(targetTags, tags) {
   });
 }
 
-const allTags = proxies.map(p => p.tag);
-pushToTargets(['all', 'all-auto'], allTags);
+const all = proxies.map(p => p.tag);
+pushTo(['🌍 Auto'], all);
 
 const reUS = /(?:🇺🇸|united\s*states|\busa\b|(^|\W)us(\W|$)|america|ashburn|new\s*york|los\s*angeles|dallas|miami|chicago|seattle|\blax\b|\biad\b|\bord\b|\bsea\b|\bdfw\b|\batl\b)/i;
 const reNL = /(?:🇳🇱|netherlands|amsterdam|rotterdam|eindhoven|\bnl\b|(^|\W)nl(\W|$)|\bams\b)/i;
 const rePL = /(?:🇵🇱|poland|warsaw|warszawa|wroclaw|wrocław|krakow|kraków|poznan|poznań|gdansk|gdańsk|\bpl\b|(^|\W)pl(\W|$))/i;
 
-const usTags = proxies.filter(p => reUS.test(p.tag)).map(p => p.tag);
-const nlTags = proxies.filter(p => reNL.test(p.tag)).map(p => p.tag);
-const plTags = proxies.filter(p => rePL.test(p.tag)).map(p => p.tag);
-
-pushToTargets(['🇺🇸 USA (Auto)'], usTags);
-pushToTargets(['🇳🇱 NL (Auto)'], nlTags);
-pushToTargets(['🇵🇱 PL (Auto)'], plTags);
+pushTo(['🇺🇸 USA (Auto)'], proxies.filter(p => reUS.test(p.tag)).map(p => p.tag));
+pushTo(['🇳🇱 NL (Auto)'], proxies.filter(p => reNL.test(p.tag)).map(p => p.tag));
+pushTo(['🇵🇱 PL (Auto)'], proxies.filter(p => rePL.test(p.tag)).map(p => p.tag));
 
 let compatAdded = false;
 config.outbounds.forEach(o => {
